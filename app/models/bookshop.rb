941448436ff04f5307302122b8728c086debdfed
alexeykis1999 <alexeykis1992@mail.ru>
2021-09-04 14:56:54 +0300

    scope to bookshop

--- app/models/bookshop.rb
+++ app/models/bookshop.rb
@@ -1,4 +1,5 @@
 class Bookshop < ApplicationRecord
+	scope :find_book , ->(name) { where(id: Book.where("name = ?", name).pluck(:bookshop_id)).pluck(:name)}
 	has_many :books, foreign_key: true
 	has_many :publishers, foreign_key: true
 end
--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,5 +1,5 @@
 class Publisher < ApplicationRecord
-	scope :find_book , ->(name) { where(id: Book.where("name = ?", name).pluck(:publisher_id)).pluck(:name)}
+
 	has_many :books, foreign_key: true
 	has_many :bookshops, foreign_key: true
 end
